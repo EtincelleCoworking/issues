156ec3bc8edd731d41e934fe87688e939de3d5e3
Sebastien Hordeaux <shordeaux@waterproof.fr>
2019-08-08 18:49:12 +0200

    initial import

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -32,12 +32,15 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <!-- Left Side Of Navbar -->
                 <ul class="navbar-nav mr-auto">
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('issues') }}">{{ __('Issues') }}</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('issue_create') }}">{{ __('Add issue') }}</a>
-                    </li>
+                    @guest
+                    @else
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('issues') }}">{{ __('Issues') }}</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('issue_create') }}">{{ __('Add issue') }}</a>
+                        </li>
+                    @endguest
                 </ul>
 
                 <!-- Right Side Of Navbar -->
