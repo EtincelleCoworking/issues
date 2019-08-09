caaca0c66b6033807d13d41f38e2402e8e946de3
Sebastien Hordeaux <shordeaux@waterproof.fr>
2019-08-09 06:36:34 +0200

    initial import

--- resources/js/components/Issues.vue
+++ resources/js/components/Issues.vue
@@ -2,7 +2,7 @@
     <b-card no-body>
         <b-tabs card>
             <b-tab title="Open issues" active>
-                <b-card-text v-if="issues.length === 0">No Issues</b-card-text>
+                <b-card-text v-if="open_issues.length === 0">No Issues</b-card-text>
                 <table class="table table-striped table-bordered" v-else>
                     <thead>
                     <tr>
@@ -15,7 +15,7 @@
                     </tr>
                     </thead>
                     <tbody>
-                    <tr v-for="issue in issues">
+                    <tr v-for="issue in open_issues">
                         <td>{{issue.id}}</td>
                         <td><a :href="'/issue/' + issue.id">{{issue.title}}</a></td>
                         <td>
@@ -29,7 +29,31 @@
                 </table>
             </b-tab>
             <b-tab title="Closed issues">
-                <b-card-text>No Issues</b-card-text>
+                <b-card-text v-if="closed_issues.length === 0">No Issues</b-card-text>
+                <table class="table table-striped table-bordered" v-else>
+                    <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Title</th>
+                        <th>Author</th>
+                        <th>Status</th>
+                        <th>Created</th>
+                        <th>Updated</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr v-for="issue in closed_issues">
+                        <td>{{issue.id}}</td>
+                        <td><a :href="'/issue/' + issue.id">{{issue.title}}</a></td>
+                        <td>
+                            <img :src="issue.reporter.avatar" class="rounded" :title="issue.reporter.name"/>
+                        </td>
+                        <td>{{issue.status}}</td>
+                        <td>{{issue.created_at | moment("dddd, MMMM Do YYYY")}}</td>
+                        <td>{{issue.updated_at | moment("dddd, MMMM Do YYYY")}}</td>
+                    </tr>
+                    </tbody>
+                </table>
             </b-tab>
         </b-tabs>
 
@@ -45,6 +69,20 @@
         },
         data() {
             return {}
+        },
+        computed: {
+            open_issues: function () {
+                return  _.without(_.map(this.issues, function(i) {
+                    if (i.status !== "Closed") return i;
+                }), undefined)
+
+            },
+            closed_issues: function () {
+                return  _.without(_.map(this.issues, function(i) {
+                    if (i.status === "Closed") return i;
+                }), undefined)
+
+            }
         },
         beforeDestroy() {
         },
